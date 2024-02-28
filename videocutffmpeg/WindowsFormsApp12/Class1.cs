640f71b1eec454b28cc6633e3547cef32b19b589
Umut Emre Demir <u.emredemir@gmail.com>
2024-02-28 11:24:41 +0300

    videocut app with ffmpeg

---
+++ videocutffmpeg/WindowsFormsApp12/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using FFmpeg;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        var inputVideo = new MediaFile("selam.mp4");
+
+      
+        var startTime = TimeSpan.FromSeconds(10); 
+        var duration = TimeSpan.FromSeconds(20);  
+
+        
+        var outputVideo = new MediaFile("selam3.mp4");
+
+      
+        var ffmpeg = new Engine();
+        var options = new ConversionOptions { Seek = startTime, Output = outputVideo, Duration = duration };
+
+        
+        ffmpeg.Convert(inputVideo, outputVideo, options);
+
+        Console.WriteLine("Video kesme işlemi tamamlandı.");
+    }
+}
